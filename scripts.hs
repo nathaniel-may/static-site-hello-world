8010f5117e729f46fe813bb9ad7480a28b9b6ff0
Nathaniel May <github@nathanielmay.com>
2024-03-07 22:14:48 -0500

    use haskell in interpreted mode

--- scripts.hs
+++ scripts.hs
@@ -1,8 +1,12 @@
+#!/usr/bin/env stack
+-- stack --install-ghc runghc --package turtle
+
 {-# LANGUAGE LambdaCase, OverloadedStrings #-}
 
 module Main where
 
 import Options.Applicative
+import System.Exit (ExitCode(..))
 import Turtle
 
 data Command = Build | Serve | Develop | Clean deriving (Read, Show, Eq)
@@ -16,7 +20,14 @@ main = do
 
 run :: MonadIO m => Command -> m ExitCode
 run = liftIO . \case
-    Build -> proc "spago" ["build"] empty
+    Build -> do
+        proc "rm" ["-rf", "dist"] empty
+            -- (.&&.) proc "cp" ["src/{index.html,live.js}", "dist/"] 
+            -- (.&&.) proc "cp" ["assets/images", "dist/"]
+            -- (.&&.) cp "assets/favicon/*" "dist/"
+            -- todo webp image conversion here with `map`
+        proc "spago" ["build"] empty
+    -- "rm -rf dist && mkdir dist && cp src/{index.html,live.js} dist/ && mkdir dist/assets/ && cp -r assets/images dist/assets && cp assets/favicon/* dist/ && for file in dist/assets/images/*; do cwebp -quiet -q 80 \"$file\" -o \"${file%.*}.webp\"; done & npx tailwindcss -c tailwind.config.js -i ./src/style.css -o ./dist/style.css & spago bundle-app --to dist/index.js",
 
     Serve -> ExitSuccess <$ print "todo implement serve"
     Develop -> ExitSuccess <$ print "todo implement develop"
