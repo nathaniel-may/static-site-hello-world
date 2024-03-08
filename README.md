6fb66fb76f6227955ece205d874fb2001c89aa0c
Nathaniel May <github@nathanielmay.com>
2024-03-08 01:46:03 -0500

    working on build command

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# static-site-hello-world
+# [WIP] static-site-hello-world
 Web dev is hard. This stack gives you more tools to deal with the suck. Mostly just a template for me to copy when I make new sites.
 
 ## Philosophy
@@ -7,7 +7,7 @@ Software has bugs. The question is when do you find them?
 From most Undesirable to most Desirable places to catch your bugs and exceptions:
 Production, user-detected ➤ Production, self-detected ➤ CD ➤ Nightly CI ➤ CI ➤ develop time ➤ impossible to write
 
-Every one of the tools in this stack pushes a whole class of errors to be caught in a more desirable stage.
+Every one of the tools in this stack pushes a whole class of errors to be caught down to a more desirable stage.
 
 ## Map
 TODO put an image here
--- scripts.hs
+++ scripts.hs
@@ -5,37 +5,72 @@
 
 module Main where
 
+import Control.Concurrent.Async (mapConcurrently_)
+import Control.Monad (when)
+import Data.List (foldl')
+import Data.Maybe (fromMaybe)
+import qualified Data.Text as T
 import Options.Applicative
 import System.Exit (ExitCode(..))
 import Turtle
 
 data Command = Build | Serve | Develop | Clean deriving (Read, Show, Eq)
 
-main :: IO ExitCode
+main :: IO ()
 main = do
-    cmd <- customExecParser (prefs showHelpOnEmpty) (info
-        (parser <**> helper)
-        (fullDesc <> progDesc "Tools for developing and deploying your application"))
+    cmd <- customExecParser (prefs showHelpOnEmpty) parser
     run cmd
 
-run :: MonadIO m => Command -> m ExitCode
+-- I use `proc "mkdir" ..` instead of `mkdir` etc. becuase turtle's commands pass over failures by returning () instead of ExitCode
+run :: MonadIO m => Command -> m ()
 run = liftIO . \case
     Build -> do
-        proc "rm" ["-rf", "dist"] empty
-            -- (.&&.) proc "cp" ["src/{index.html,live.js}", "dist/"] 
-            -- (.&&.) proc "cp" ["assets/images", "dist/"]
-            -- (.&&.) cp "assets/favicon/*" "dist/"
-            -- todo webp image conversion here with `map`
-        proc "spago" ["build"] empty
-    -- "rm -rf dist && mkdir dist && cp src/{index.html,live.js} dist/ && mkdir dist/assets/ && cp -r assets/images dist/assets && cp assets/favicon/* dist/ && for file in dist/assets/images/*; do cwebp -quiet -q 80 \"$file\" -o \"${file%.*}.webp\"; done & npx tailwindcss -c tailwind.config.js -i ./src/style.css -o ./dist/style.css & spago bundle-app --to dist/index.js",
+        let npmInstall = procs "npm" [ "install" ] empty
+        let moveStuff = do
+                procs "rm" [ "-rf", "dist" ] empty
+                procs "mkdir" [ "dist/"] empty
+                -- todo generate index.html (it's not compiled so it should live somewhere speical or be generated)
+                procs "cp" [ "src/index.html", "dist/" ] empty
+                -- this is a special case of uncompiled code. it should go in a directory of raw js somewhere
+                procs "cp" [ "src/live.js", "dist/" ] empty
+                procs "cp" [ "-r", "assets/images", "dist/" ] empty
+                -- todo: the * doesn't resolve properly
+                -- procs "cp" [ "assets/favicon/*", "dist/" ] empty
+                -- todo: for file in dist/assets/images/*; do cwebp -quiet -q 80 \"$file\" -o \"${file%.*}.webp\"; done
+                -- use this pattern: pages <- forConcurrently ["url1", "url2", "url3"] $ \url -> getURL url
+        let buildStyles = procs "npx" [ "tailwindcss"
+                , "-c", "tailwind.config.js"
+                , "-i", "./src/style.css"
+                , "-o", "./dist/style.css" ] empty
+        let buildJS = procs "spago" [ "bundle-app", "--to", "dist/index.js" ] empty
+        mapConcurrently_ id [ npmInstall, moveStuff, buildStyles, buildJS ]
 
-    Serve -> ExitSuccess <$ print "todo implement serve"
-    Develop -> ExitSuccess <$ print "todo implement develop"
-    Clean -> ExitSuccess <$ print "todo implement clean"
+    Serve   -> print "todo implement serve"
+        -- look at pushd for serving in a different directory
+    Develop -> print "todo implement develop"
+    Clean   -> print "todo implement clean"
 
-parser :: Parser Command
-parser = subparser
-     ( command "build"  (info (pure Build) ( progDesc "build everything" ))
-    <> command "serve"  (info (pure Serve) ( progDesc "serve the web app" ))
-    <> command "serve"  (info (pure Serve) ( progDesc "serve the web app" ))
-    <> command "serve"  (info (pure Serve) ( progDesc "serve the web app" )) )
+parser :: ParserInfo Command
+parser = info
+    (subcommands <**> helper)
+    (fullDesc <> progDesc "Tools for developing and deploying your application")
+    where
+    subcommands :: Parser Command
+    subcommands = subparser
+        (  command "build"   (info (pure Build)   ( progDesc "build everything" ))
+        <> command "serve"   (info (pure Serve)   ( progDesc "serve the web app" ))
+        <> command "develop" (info (pure Develop) ( progDesc "serve and watch for source file changes to reload the page" ))
+        <> command "clean"   (info (pure Clean)   ( progDesc "delete all build files" )) )
+
+-- test :: IO ExitCode
+-- test = do
+--     let tests =
+--             [ do
+--                 actual <- execParserPure "build" parser
+--                 when (Build /= actual) $ throw "bad build command parse"
+--             , do
+--                 actual <- execParserPure "serve" parser
+--                 when (Build /= actual) $ throw "bad serve command parse"
+--             ]
+
+--     mapConcurrently_ tests
