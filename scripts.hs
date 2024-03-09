554e2d1fe42c92423883299aff98be954a62ba3e
Nathaniel May <github@nathanielmay.com>
2024-03-09 01:37:02 -0500

    addressed warnings in build script

--- scripts.hs
+++ scripts.hs
@@ -33,47 +33,50 @@ main = do
 -- I use `proc "mkdir" ..` instead of `mkdir` etc. becuase turtle's commands pass over failures by returning () instead of ExitCode
 run :: MonadIO m => App -> m ()
 run = liftIO . \case
-    Run Build -> let
-        npmInstall = procs "npm" [ "install" ] empty
-        moveStuff = do
-            procs "rm" [ "-rf", "dist" ] empty
-            procs "mkdir" [ "-p", "dist/images"] empty
-            -- todo generate index.html (it's not compiled so it should live somewhere speical or be generated)
-            procs "cp" [ "src/index.html", "dist/" ] empty
-            -- this is a special case of uncompiled code. it should go in a directory of raw js somewhere
-            procs "cp" [ "src/live.js", "dist/" ] empty
-            procs "cp" [ "-r", "assets/images", "dist/" ] empty
-            -- bash: cp assets/favicon/* dist/
-            sh (do file <- ls "assets/favicon/"; liftIO (procs "cp" [ T.pack file, "dist/" ] empty ))
-            -- bash: for file in dist/images/*; do cwebp -quiet -q 80 \"$file\" -o \"${file%.*}.webp\"; done
-            -- TODO do concurrently?
-            sh (do file <- ls "dist/images/"; liftIO (procs "cwebp" [ "-quiet", "-q", "80", T.pack file, "-o", T.pack $ replaceExtension file "webp" ] empty ))
-        buildStyles = procs "npx" [ "tailwindcss"
-            , "-c", "tailwind.config.js"
-            , "-i", "./src/style.css"
-            , "-o", "./dist/style.css" ] empty
-        buildJS = procs "npm" [ "run", "spago-bundle-app" ] empty
-        in mapConcurrently_ id [ npmInstall, moveStuff, buildStyles, buildJS ]
+    -- runs the tests before every command execution
+    Run cmd -> run Test *> case cmd of
+        Build -> let
+            npmInstall = procs "npm" [ "install" ] empty
+            moveStuff = do
+                procs "rm" [ "-rf", "dist" ] empty
+                procs "mkdir" [ "-p", "dist/images"] empty
+                -- todo generate index.html (it's not compiled so it should live somewhere speical or be generated)
+                procs "cp" [ "src/index.html", "dist/" ] empty
+                -- this is a special case of uncompiled code. it should go in a directory of raw js somewhere
+                procs "cp" [ "src/live.js", "dist/" ] empty
+                procs "cp" [ "-r", "assets/images", "dist/" ] empty
+                -- bash: cp assets/favicon/* dist/
+                sh (do file <- ls "assets/favicon/"; liftIO (procs "cp" [ T.pack file, "dist/" ] empty ))
+                -- bash: for file in dist/images/*; do cwebp -quiet -q 80 \"$file\" -o \"${file%.*}.webp\"; done
+                -- TODO do concurrently?
+                sh (do file <- ls "dist/images/"; liftIO (procs "cwebp" [ "-quiet", "-q", "80", T.pack file, "-o", T.pack $ replaceExtension file "webp" ] empty ))
+            buildStyles = procs "npx" [ "tailwindcss"
+                , "-c", "tailwind.config.js"
+                , "-i", "./src/style.css"
+                , "-o", "./dist/style.css" ] empty
+            -- calling npm run instead of spago bundle-app directly so spago can find `esbuild` in local node_modules
+            buildJS = procs "npm" [ "run", "spago-bundle-app" ] empty
+            in mapConcurrently_ id [ npmInstall, moveStuff, buildStyles, buildJS ]
 
-    Run Serve   -> print "todo implement serve"
-        -- look at pushd for serving in a different directory
+        Serve   -> print "todo implement serve"
+            -- look at pushd for serving in a different directory
 
-    Run Develop -> print "todo implement develop"
+        Develop -> print "todo implement develop"
 
-    Run Install -> print "todo implement install"
-        -- npm i
+        Install -> print "todo implement install"
+            -- npm i
 
-    Run Clean -> let
-        dirs =
-            [ "dist/"
-            , "node_modules/"
-            , "output/"
-            , ".spago/"
-            , ".stack-work/"
-            ]
-        in forConcurrently_ dirs (\dir ->
-            procs "rm" [ "-rf", dir ] empty
-        )
+        Clean -> let
+            dirs =
+                [ "dist/"
+                , "node_modules/"
+                , "output/"
+                , ".spago/"
+                , ".stack-work/"
+                ]
+            in forConcurrently_ dirs (\dir ->
+                procs "rm" [ "-rf", dir ] empty
+            )
 
     Test -> let
         tests =
--- spago.dhall
+++ spago.dhall
@@ -18,7 +18,6 @@ to generate this file without the comments in this block.
   , "exceptions"
   , "halogen"
   , "maybe"
-  , "node-fs"
   , "prelude"
   , "transformers"
   , "web-dom"
