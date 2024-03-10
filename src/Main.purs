3cee4f2f97fe98b9de072a0c553e78c26e8c5b34
Nathaniel May <github@nathanielmay.com>
2024-03-10 19:46:19 -0400

    cleaner rerendering with combined root component

--- src/Main.purs
+++ src/Main.purs
@@ -4,6 +4,7 @@ import Prelude
 
 import Control.Monad.Error.Class (throwError)
 import Data.Foldable (traverse_)
+import Data.Int.Bits (xor)
 import Data.Maybe (maybe)
 import Data.Time.Duration (Milliseconds(..))
 import Effect (Effect)
@@ -24,41 +25,19 @@ import Web.HTML.HTMLElement (HTMLElement, toElement)
 main :: Effect Unit
 main = HA.runHalogenAff do
   HA.awaitLoad
-  head <- selectElement_ "head"
-  body <- selectElement_ "body"
+  html <- selectElement_ "html"
   -- clear any existing values and re render. 
   -- initial html served should contain references to all the expensive assets so they can
   -- take advantage of browser initial page loading optimiations
-  liftEffect $ replaceChildren (toElement head) []
-  liftEffect $ replaceChildren (toElement body) []
-  traverse_ (\elem -> runUI (headComponent elem) unit head) headContents
-  runUI bodyComponent unit body
+  liftEffect $ replaceChildren (toElement html) []
+  -- add each child of html (i.e. - head, body as described in headContents) as its own component
+  traverse_ (\elem -> runUI (rootComponent elem) unit html) rootContents
 
 -- nodeMain :: Effect Unit
 -- nodeMain = ???
 
-bodyComponent ∷ ∀ q i o m. MonadAff m => H.Component q i o m
-bodyComponent =
-  H.mkComponent
-    { initialState: const unit
-    , render
-    , eval: H.mkEval $ H.defaultEval { handleAction = pure }
-    }
-  where
-  render :: ∀ state action slots. state -> H.ComponentHTML action slots m
-  render _ =
-    HH.div
-      [ css "h-full leading-relaxed font-light text-lg text-warm-gray" ]
-      [ HH.div
-        [ css "h-full w-full flex justify-center items-center" ]
-        [ HH.h1
-          [ css "font-7xl font-black" ]
-          [ HH.text "Hello World" ]
-        ]
-      ]
-
-headComponent :: ∀ a q i o m. HH.HTML (H.ComponentSlot a m Unit) Unit -> H.Component q i o m
-headComponent elem =
+rootComponent :: ∀ a q i o m. HH.HTML (H.ComponentSlot a m Unit) Unit -> H.Component q i o m
+rootComponent elem = 
   H.mkComponent
     { initialState
     , render
@@ -66,10 +45,21 @@ headComponent elem =
     }
   where
   initialState _ = elem
-
   render state = state
-  
-headContents = [ HH.title_ [ HH.text "Hello World" ] ]
+
+rootContents =
+  [ HH.head_
+    [ HH.title_ [ HH.text "Hello World" ] ]
+  , HH.body
+    [ css "h-full leading-relaxed font-light text-lg text-warm-gray"]
+    [ HH.div
+      [ css "h-full w-full flex justify-center items-center" ]
+      [ HH.h1
+        [ css "font-7xl font-black" ]
+        [ HH.text "Hello World" ]
+      ]
+    ]
+  ]
 
 css :: ∀ r i. String -> HH.IProp (class :: String | r) i
 css = HP.class_ <<< HH.ClassName
