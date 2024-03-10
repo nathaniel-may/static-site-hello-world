94223b576544b12c0fb0e2fa860f30eef64021b2
Nathaniel May <github@nathanielmay.com>
2024-03-09 21:25:57 -0500

    fixed previous issue with build script. has new issue.

--- scripts.hs
+++ scripts.hs
@@ -11,7 +11,7 @@ import Control.Monad (when)
 import Data.ByteString (ByteString)
 import Data.List (foldl')
 import Data.Foldable (traverse_)
-import Data.Maybe (fromMaybe, mapMaybe)
+import Data.Maybe (catMaybes, fromMaybe, mapMaybe)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import Options.Applicative
@@ -47,15 +47,21 @@ run = liftIO . \case
                 -- dist is the directory the server serves from
                 procs "rm" [ "-rf", "root", "dist" ] empty
                 procs "mkdir" [ "-p", "root", "dist/images"] empty
-                -- get every file in the project root so it can be moved to its pre-build location
-                paths <- map T.pack <$> listShell (ls ".")
+                -- todo generate index.html (it's not compiled so it should live somewhere special or be generated)
                 -- copy whole dirs with their existing structure before moving individual files
-                let dirMoves = [ ("./src", "./root/"), ("./test", "./root/") ]
-                let fileMoves = mapMaybe (\path -> (path, ) <$> if
+                let dirMoves = [ ("./src", "root"), ("./test", "root"), ("./assets/images", "dist") ]
+                traverse_ (\(src, dest) -> procs "cp" [ "-r", src, dest ] empty) dirMoves
+                -- for each file in the source directory, move it or leave it
+                ls "." >>= (\path' -> let
+                    path = T.pack path'
+                    move = (path,) <$> if
                         -- skip generated files
                         | any (`T.isPrefixOf` path) builtPathsText -> Nothing
-                        -- if we have a destination for the whole directory, don't move the individual files
+                        -- if we already copied the whole directory, don't move the individual files
                         | any ((`T.isPrefixOf` path) . fst) dirMoves -> Nothing
+                        -- these files move
+                        | T.isPrefixOf "./assets/favicon/" path -> Just "dist"
+                        | T.isPrefixOf "./config/" path -> Just "root"
                         -- these files don't move
                         | path `elem`
                             [ "./scripts.hs"
@@ -64,38 +70,36 @@ run = liftIO . \case
                             , "./diagram.png"
                             , "./.purs-repl"
                             , "./.psc-ide-port"
+                            , "./.gitignore"
+                            , "./.git"
+                            -- these top-level directories doesn't move
+                            , "./config"
+                            , "./assets" 
                             ] -> Nothing
-                        -- these files move
-                        | T.isPrefixOf "./assets/images" path -> Just "dist/images"
-                        | T.isPrefixOf "./assets/favicon" path -> Just "dist/"
-                        | T.isPrefixOf "./config" path -> Just "root/"
                         -- if the path hasn't been explicitly mapped, raise an error and don't build so every file is accounted for
                         | otherwise ->
-                            error $ "unknown source file: " <> T.unpack path
+                            error $ "unknown source file: " <> path'
                                 <> "\nPlease designate its pre-build destination."
-                        )
-                        paths
-                let boop = (fileMoves :: [(Text, Text)])
-                traverse_ (\(src, dest) -> procs "cp" [ "-r", src, dest ] empty) dirMoves
-                traverse_ (\(src, dest) -> procs "cp" [ src, dest ] empty) fileMoves
-                -- todo generate index.html (it's not compiled so it should live somewhere special or be generated)
-
+                    in case move of
+                        Just (src, dest) -> do echo (unsafeTextToLine src); procs "cp" [ src, dest ] empty
+                        Nothing -> pure ()
+                    )
             -- bash: for file in dist/images/*; do cwebp -quiet -q 80 \"$file\" -o \"${file%.*}.webp\"; done
-            buildImages = sh (do file <- ls "dist/images/"; liftIO (procs "cwebp" [ "-quiet", "-q", "80", T.pack file, "-o", T.pack $ replaceExtension file "webp" ] empty ))
+            buildImages = do file <- ls "dist/images/"; procs "cwebp" [ "-quiet", "-q", "80", T.pack file, "-o", T.pack $ replaceExtension file "webp" ] empty
             buildStyles = procs "npx" [ "tailwindcss"
                 , "-c", "tailwind.config.js"
                 , "-i", "./root/src/style.css"
                 , "-o", "./dist/style.css" ] empty
             -- calling npm run instead of spago bundle-app directly so spago can find `esbuild` in local node_modules
-            buildJS = sh (pushd "./root" >> inprocWithErr "npm" [ "run", "spago-bundle-app" ] empty)
-            in moveStuff >> run (Run Install) >> mapConcurrently_ id [ buildStyles, buildJS, buildImages ]
+            buildJS = pushd "./root" >> procs "npm" [ "run", "spago-bundle-app" ] empty
+            in view $ do view moveStuff; run (Run Install); liftIO $ mapConcurrently_ view [ buildStyles, buildJS, buildImages ]
 
         Serve   -> print "todo implement serve"
             -- look at pushd for serving in a different directory
 
         Develop -> print "todo implement develop"
 
-        Install -> sh (pushd "./root" >> inprocWithErr "npm" [ "install" ] empty)
+        Install -> view $ pushd "./root" >> procs "npm" [ "install" ] empty
 
         -- todo overkill to concurrently do this
         Clean -> forConcurrently_ builtPaths (\dir -> procs "rm" [ "-rf", T.pack dir ] empty )
@@ -110,15 +114,15 @@ run = liftIO . \case
         in do
             mapConcurrently_ id tests
 
--- leading "./" is necessary to match out put of find
+-- leading "./" and lack of trailing "/" is necessary to match out put of find
 builtPaths :: [FilePath]
 builtPaths =
-    [ "./dist/"
-    , "./node_modules/"
-    , "./output/"
-    , "./.spago/"
-    , "./.stack-work/"
-    , "./root/"
+    [ "./dist"
+    , "./node_modules"
+    , "./output"
+    , "./.spago"
+    , "./.stack-work"
+    , "./root"
     ]
 
 builtPathsText :: [Text]
@@ -138,11 +142,11 @@ parser = info
         <> command "clean"        (info (pure $ Run Clean)   ( progDesc "delete all build files" ))
         <> command "test-scripts" (info (pure Test)          ( progDesc "test all the scripts in this file" )) )
 
-listShell :: Shell a -> IO [a]
-listShell = flip foldShell (FoldShell (pure . pure) [] pure)
-
 for :: Functor f => f a -> (a -> b) -> f b
 for = flip fmap
+
+for_  :: Functor f => f a -> (a -> b) -> f ()
+for_ x f = void $ fmap f x
 
 test :: MonadIO m => Text -> m a -> (a -> Bool) -> Text -> m ()
 test name actual fExpected msg = do
